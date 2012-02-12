b0b88014a77058476362f4c67d56ce5bba74abd5
Timothy Kempf <the.kaempfer@gmail.com>
2012-02-11 21:20:49 -0800

    Enabled basic asset concatenation and upload to Amazon S3.

--- examples/basic.js
+++ examples/basic.js
@@ -1,3 +1,23 @@
+var express = require('express');
+var server = express.createServer();
+server.listen( 8080 );
+
 var igneous = require('../lib/igneous.js');
 
-console.log( igneous.createFlows );
+igneous.set( 's3_key', 'S3 KEY' );
+igneous.set( 's3_secret', 'S3 SECRET' );
+igneous.set( 's3_bucket', 'S3 BUCKET' );
+
+igneous.createFlows({
+	'stylesheets.css': {
+		type: 'css',
+		files: [
+			'styles/layout.css',
+			'styles/red.css'
+		]
+	}
+});
+
+server.get( '/', function( req, res ){
+	res.send('<html><head><link rel="stylesheet" href="'+ igneous.flows['stylesheets.css'].url +'" /></head></html>');
+});
