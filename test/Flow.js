78fdfe8c7606c24968ca0109eeda3217c730daea
Fauntleroy <the.kaempfer@gmail.com>
2012-10-30 23:09:22 -0700

    add tests for store and flow

--- test/Flow.js
+++ test/Flow.js
@@ -1,23 +1,53 @@
 var should = require('should');
+var sinon = require('sinon');
 var Flow = require('../lib/flow.js');
 
 describe('Flow', function(){
 
+	var config = {
+		route: 'test.js',
+		type: 'js',
+		base: 'scripts',
+		paths: 'test.js',
+		preprocessors: ['coffeescript'],
+		postprocessors: ['minify']
+	};
+	var igneous_config = {
+		root: __dirname +'/fixtures',
+		store: {
+			save: sinon.spy(),
+			get: sinon.spy()
+		}
+	};
+	var flow = new Flow( config, igneous_config );
+
 	it( 'sets appropriate MIME types based on type', function(){
+
+		flow.mime_type.should.equal('text/javascript');
 
 	});
 
 	it( 'converts preprocessor strings to methods', function(){
 
+		flow.config.preprocessors[0].should.be.a('function');
+
 	});
 
 	it( 'converts postprocessor strings to methods', function(){
+
+		flow.config.postprocessors[0].should.be.a('function');
 
 	});
 
 	describe( '.flow', function(){
 
 		it( 'regenerates the flow\'s store', function(){
+
+			sinon.spy( flow, 'save' );
+			flow.flow();
+
+			flow.save.called.should.be.true;
+			flow.save.restore();
 
 		});
 
@@ -27,13 +57,34 @@ describe('Flow', function(){
 
 		it( 'reads a single file', function(){
 
+			flow.config.paths = ['test.js'];
+			flow.add();
+			var files_length = 0;
+			for( var key in flow.files ) files_length++;
+
+			files_length.should.equal(1);
+
 		});
 
 		it( 'reads a directory of files', function(){
 
+			flow.config.paths = ['test'];
+			flow.add();
+			var files_length = 0;
+			for( var key in flow.files ) files_length++;
+
+			files_length.should.equal(3);
+
 		});
 
 		it( 'excludes files based on extension', function(){
+
+			flow.config.paths = ['test'];
+			flow.add();
+			var files_length = 0;
+			for( var key in flow.files ) files_length++;
+
+			files_length.should.equal(3);
 
 		});
 
@@ -43,6 +94,12 @@ describe('Flow', function(){
 
 		it( 'runs files through each specified preprocessor', function(){
 
+			var preprocessor = sinon.spy();
+			flow.config.preprocessors = [preprocessor];
+			flow.preprocess();
+
+			preprocessor.called.should.be.true;
+
 		});
 
 	});
@@ -50,6 +107,33 @@ describe('Flow', function(){
 	describe( '.concatenate', function(){
 
 		it( 'joins all file data together into a single string', function(){
+
+			flow.files = {
+				test: {
+					contents: 'test = true;'
+				},
+				test2: {
+					contents: 'test2 = true;'
+				}
+			};
+			var concatenated = flow.files.test.contents +'\r\n'+ flow.files.test2.contents +'\r\n';
+			flow.concatenate();
+
+			flow.data.should.equal( concatenated );
+
+		});
+
+	});
+
+	describe( '.postprocess', function(){
+
+		it( 'runs files through each specified postprocessor', function(){
+
+			var postprocessor = sinon.spy();
+			flow.config.postprocessors = [postprocessor];
+			flow.postprocess();
+
+			postprocessor.called.should.be.true;
 
 		});
 
@@ -59,25 +143,13 @@ describe('Flow', function(){
 
 		it( 'sends file data to be saved by the specified store', function(){
 
-		});
+			flow.data = '';
+			flow.store.save.reset();
+			flow.save();
 
-	});
+			flow.store.save.called.should.be.true;
 
-	describe( '.watch', function(){
-
-		it( 'watches all paths', function(){
-
-		});
-
-		it( 'reflows flow on file change', function(){
-
-		});
-
-		it( 'reflows flow on file addition', function(){
-
-		});
-
-		it( 'reflows flow on file removal', function(){
+			flow.store.save.reset();
 
 		});
 
--- test/stores/memory.js
+++ test/stores/memory.js
@@ -1,11 +1,27 @@
 var should = require('should');
-var Flow = require('../../lib/stores/memory.js');
+var sinon = require('sinon');
+var MemoryStore = require('../../lib/stores/memory.js');
 
 describe( 'MemoryStore', function(){
 
+	var memory = new MemoryStore();
+
 	describe( '.save', function(){
 
-		it( 'stores file data in memory', function(){
+		it( 'stores file data in memory', function( cb ){
+
+			var file = {
+				id: 1,
+				data: 'test'
+			};
+			var callback = sinon.spy();
+
+			memory.save( file, function(){
+
+				memory.files[file.id].should.equal(file.data);
+				cb();
+
+			});
 
 		});
 
@@ -13,7 +29,17 @@ describe( 'MemoryStore', function(){
 
 	describe( '.get', function(){
 
-		it( 'retrieves file data from memory', function(){
+		it( 'retrieves file data from memory', function( cb ){
+
+			memory.files = {};
+			memory.files[1] = 'test';
+
+			memory.get( 1, function( data ){
+
+				data.should.equal(memory.files[1]);
+				cb();
+
+			});
 
 		});
 
